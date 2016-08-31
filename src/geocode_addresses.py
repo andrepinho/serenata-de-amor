1969d5552ebb9996546396c5c452051ee9d4d69c
Irio Musskopf <iirineu@gmail.com>
2016-08-30 21:28:12 -0300

    Ongoing progress of performing concurrent geocoding

--- src/geocode_addresses.py
+++ src/geocode_addresses.py
@@ -1,3 +1,4 @@
+from concurrent import futures
 from geopy.exc import GeocoderQuotaExceeded, GeocoderTimedOut
 from geopy.geocoders import GoogleV3
 import pickle
@@ -9,6 +10,7 @@ DATASET_PATH = 'data/companies.xz'
 TEMP_PATH = 'data/companies'
 
 def write_company_geocoding(company_location, cnpj):
+    cnpj = re.sub(r'[./-]', "", cnpj)
     print('Writing %s' % cnpj)
     with open('%s/%s.pkl' % (TEMP_PATH, cnpj), 'wb') as f:
         pickle.dump(company_location, f, pickle.HIGHEST_PROTOCOL)
@@ -17,23 +19,12 @@ def fetch_company_coordinates(company):
     address = ' '.join(company[['address', 'number', 'zip_code', 'neighborhood', 'city', 'state']].dropna())
     if address == '':
         print('No address information for', company['name'], company['cnpj'])
-        return pd.Series()
+        return None
     else:
-        print(address)
-        try:
-            location = geolocator.geocode(address)
-        except GeocoderTimedOut as e:
-            print('Timeout with %s' % address)
-            return pd.Series()
+        location = geolocator.geocode(address)
+        return location
 
-        if location:
-            cnpj = re.sub(r'[./-]', "", company['cnpj'])
-            write_company_geocoding(location, cnpj)
-            return pd.Series({'latitude': location.latitude,
-                              'longitude': location.longitude})
-        else:
-            print('No coordinates found for', company['name'], company['cnpj'], address)
-            return pd.Series()
+
 
 if not os.path.exists(TEMP_PATH):
     os.makedirs(TEMP_PATH)
@@ -44,11 +35,25 @@ data = pd.read_csv(DATASET_PATH, low_memory=False)
 geocoding_already_fetched = [filename[:14]
                    for filename in os.listdir(TEMP_PATH)
                    if filename.endswith('.pkl')]
-
 remaining_companies = data[~data['cnpj'].str.replace(r'[./-]', ''). \
     isin(geocoding_already_fetched)]
 
 print(len(data), len(remaining_companies))
+
+with futures.ThreadPoolExecutor(max_workers=20) as executor:
+    future_to_geocoding = dict()
+    for index, company in remaining_companies.iterrows():
+        future = executor.submit(fetch_company_coordinates, company)
+        future_to_geocoding[future] = company
+
+    for future in futures.as_completed(future_to_geocoding):
+        company = future_to_geocoding[future]
+        if future.exception() is not None:
+            print('%r raised an exception: %s' % (company['cnpj'], future.exception()))
+        elif future.result() is not None:
+            print('saving')
+            write_company_geocoding(future.result(), company['cnpj'])
+
 data = pd.concat([remaining_companies, remaining_companies.apply(fetch_company_coordinates, axis=1)], axis=1)
 
 # data.to_csv(DATASET_PATH,
